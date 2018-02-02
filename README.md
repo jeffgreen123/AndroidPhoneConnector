12c8ac027fc7f4c703081dce2955875b5ab3a828
Jeff Green <ja4green@uwaterloo.ca>
2018-02-02 09:34:12 -0500

    updated read me and screen functionality (still much needed)

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PhoneConnector
+WORK IN PROGRESS
+</br>
+</br>
+This is an application that is esentially and android version of the windows phone app I used to connect to my local home computer. 
+</br>
+</br>
+This app starts by connecting to a socket open on my local computer by the C++ application also located on my github called Server.
+</br>
+</br>
+The android app with then send a command to intiate its intent to the server running on my local computer.
+</br>
+</br>
+The local machine server will then process any further data sent from the android app and send back data if needed.
--- app/src/main/java/com/example/myfirstapp/Screen.java
+++ app/src/main/java/com/example/myfirstapp/Screen.java
@@ -3,9 +3,12 @@ package com.example.myfirstapp;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class Screen extends AppCompatActivity {
+
+    boolean roundImage = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,21 @@ public class Screen extends AppCompatActivity {
         ImageView imgView=(ImageView) findViewById(R.id.screenImage);
         Drawable drawable  = getResources().getDrawable(R.mipmap.ic_launcher);
         imgView.setImageDrawable(drawable);
+    }
+
+    protected void changeImage(View view) {
+        ImageView imgView=(ImageView) findViewById(R.id.screenImage);
+        if(roundImage){
+            Drawable drawable  = getResources().getDrawable(R.mipmap.ic_launcher);
+            imgView.setImageDrawable(drawable);
+            roundImage = false;
+        }
+        else {
+            Drawable drawable  = getResources().getDrawable(R.mipmap.ic_launcher_round);
+            imgView.setImageDrawable(drawable);
+            roundImage = true;
+        }
+
+
     }
 }
